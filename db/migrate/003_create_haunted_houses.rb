b80da47ea307122126c50cf4ed96d96602d475bc
kiku ishimoto <kikuish@gmail.com>
2020-06-10 21:14:45 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.timedate :opening_date
-      t.timedate :closing_date
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.text :description
     end
   end
