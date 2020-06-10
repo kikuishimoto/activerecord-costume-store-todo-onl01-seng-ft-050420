bf4ab66b239036388818a762cbca0476ffa3fa4f
kiku ishimoto <kikuish@gmail.com>
2020-06-10 21:12:55 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,15 +1,15 @@
 class CreateHauntedHouse < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
-      t.string :name 
+      t.string :name
       t.string :location
-      t.string :theme 
+      t.string :theme
       t.integer :price
       t.boolean :family_friendly
       t.timedate :opening_date
       t.timedate :closing_date
       t.text :description
-    end 
+    end
   end 
-    
+
 end
